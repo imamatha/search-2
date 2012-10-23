9b9efadcd5863d6d9e45ff2926f515988c46b3c7
mamatha <munivelu.mamatha@accenture.com>
2012-10-23 15:49:17 +0530

    Update javascripts/search-content.js

--- javascripts/search-content.js
+++ javascripts/search-content.js
@@ -208,8 +208,8 @@ function expandDocument(id){
 					documentdata += response.data.subject+'</a><br/><br/>Description</font>'+response.data.content.binary.description;
 					console.log("Desc: "+response.data.content.binary.description);
 					//documentdata +='<div class="answerborder">';					
-					documentdata += '<font class="root" style="color:blue">This document contains an uploaded document (PDF/DOC).';
-					documentdata += 'Please click <a target="_app" href="'+response.data.resources.html.ref+'">here</a> to open the document</font></div>';
+					documentdata += '<span class="subtext">This document contains an uploaded document (PDF/DOC). ';
+					documentdata += 'Please click <a target="_app" href="'+response.data.resources.html.ref+'">here</a> to open the document</span></div>';
 					documentdata +='</div>';
 				}
 				else
