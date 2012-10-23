6a19fd8008fdddcdfa2d65c9661b5c80f6154920
mamatha <munivelu.mamatha@accenture.com>
2012-10-23 15:33:08 +0530

    Update javascripts/search-content.js

--- javascripts/search-content.js
+++ javascripts/search-content.js
@@ -204,7 +204,7 @@ function expandDocument(id){
 				if(isBinaryDoc !=0)
 				{
 					documentdata +='<div class="rootborder">';
-					documentdata +='<div class="document"><font size="+1"><a href="'+response.data.resources.html.ref+'"';
+					documentdata +='<div class="document"><font size="+1"><a href="'+response.data.resources.html.ref+'">';
 					documentdata += response.data.subject+'</a></font><br/>Description </div>'+response.data.content.binary.description;
 					console.log("Desc: "+response.data.content.binary.description);
 					//documentdata +='<div class="answerborder">';					
