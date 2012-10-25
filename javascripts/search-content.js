0957758f156f567c7233aa1e4695d03a9004b604
mamatha <munivelu.mamatha@accenture.com>
2012-10-25 16:25:37 +0530

    Update javascripts/search-content.js

--- javascripts/search-content.js
+++ javascripts/search-content.js
@@ -419,8 +419,7 @@ function search() {
 				  discussionImage +='<span class="jive-icon-med jive-icon-discussion"></span>';
 				}
 			
-                    		discussion +='<div id="div_'+discussionID+'" class="firstdiv">';
-                    		discussion +='<div>';
+                    		discussion +='<div id="div_'+discussionID+'" class="firstdiv">';                    		
 				discussion +='<ul>';
 				discussion +=discussionImage+'<li><a href="'+url+'" target="_apps">'+subject+'</a></li>';
                                	discussion +='</ul>'; 
@@ -440,11 +439,9 @@ function search() {
                                 discussion +='<ul>';                   
                    		discussion +='<div class="align">'+contentSummary+'</div>';                  
                     	        discussion +='</ul>';
+				discussion +='</div>';				                
 				discussion +='</div>';
-					                
-				discussion +='</div>';
-				discussion +='</div>';
-				discussion +='<br>';                
+				//discussion +='<br>';                
                            }
                
                
