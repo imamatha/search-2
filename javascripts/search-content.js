b9b554aee1c894e97e75a827a97d9f8abda4a60d
mamatha <munivelu.mamatha@accenture.com>
2012-10-30 10:32:37 +0530

    Update javascripts/search-content.js

--- javascripts/search-content.js
+++ javascripts/search-content.js
@@ -1,90 +1,87 @@
 // On-view-load initialization
 function init() {
-   
-    $("#search").click(search);
-	//$(".image-button").click(expand);
-    gadgets.window.adjustHeight();
-   
+      $("#search").click(search);
+      gadgets.window.adjustHeight();
 }
 
 $("span.image-button").live('hover', function () {
-      var curRowId = $(this).attr("id");
-	  if(curRowId.indexOf("DOC") != -1){
-		var docID = (curRowId.substring(curRowId.lastIndexOf("-"))).substr(1);
-		//alert("found :: "+docID +" "+curRowId);
-		console.log("i'm in if section:document");
-		expandDocument(docID);
-		}
-		else if(curRowId.indexOf("post") != -1){
+                var curRowId = $(this).attr("id");
+	        if(curRowId.indexOf("DOC") != -1){
+	           var docID = (curRowId.substring(curRowId.lastIndexOf("-"))).substr(1);
+	           console.log("i'm in if section:document");
+	           expandDocument(docID);
+	          }
+	        else if(curRowId.indexOf("post") != -1){
 			var blogpostId = (curRowId.substring(curRowId.lastIndexOf("-"))).substr(1);
 			console.log("i'm in if section:blogID::"+blogpostId);
 			var finalBlogId=(blogpostId.substring(blogpostId.lastIndexOf("/"))).substr(1);
 			console.log("i'm in if section:PostID::"+finalBlogId)
 			expandBlog(finalBlogId,blogpostId);
-		}
+		       }
 		else
 		{
 			console.log("i'm in else section");
 			expandDiscussion(curRowId);
 		}
 
-	
     });
     
-    
+ //function for tabs   
  $(function() {
         $( "#tabs" ).tabs();
-    });
-
+   });
+   
+//function for date format
  function monthConvert(d){
 
   var outMonth="";
     switch (d) {
-        case '01':
+    case '01':
     outMonth= "Jan";
     break;
-  case '02':
-   outMonth= "Feb";
+    case '02':
+    outMonth= "Feb";
     break;
-  case '03':
+    case '03':
     outMonth= "Mar";
     break;
-  case '04':
+    case '04':
     outMonth= "Apr";
-break;
-case '05':
+    break;
+    case '05':
     outMonth= "May";
     break;
-  case '06':
+    case '06':
     outMonth= "Jun";
     break;
-  case '07':
+    case '07':
     outMonth= "Jul";
     break;
-  case '08':
+    case '08':
     outMonth= "Aug";
-break;
-case '09':
+    break;
+    case '09':
     outMonth= "Sep";
     break;
-  case '10':
+    case '10':
     outMonth= "Oct";
     break;
-  case '11':
+    case '11':
     outMonth= "Nov";
     break;
-  case '12':
+    case '12':
     outMonth= "Dec";
-break;
+    break;
 }
 return outMonth;
 }
+
+//function for expand button to display the discussions with correct and helpful answers
 function expandDiscussion(id){
 
-	 $(".content").html("");
+	$(".content").html("");
 	$('.firstdiv').css('background-color', '#FFFFFF');
 	$('#div_'+id).css('background-color', '#F2F2F2');
-//	$('#div_'+id).css({"background-color":"#F2F2F2","padding-top":"0px","border":"1px #FFFFFF"});
 	console.log("Expand Row Id::: "+ id);
 	var discussionMessage="";
 	var correctanswer="";
@@ -94,14 +91,14 @@ function expandDiscussion(id){
 	
 	var request = osapi.jive.core.discussions.get({id: id});
 	request.execute(function(response) { 
-		console.log("Expanding discussion response is " + JSON.stringify(response.data));
-		var discussionresult=response.data;
+	console.log("Expanding discussion response is " + JSON.stringify(response.data));
+	var discussionresult=response.data;
 		
 		if (response.error) {
 			console.log("Error in get: "+response.error.message);
-		}
+		    }
+		    
 		else{
-			
 			myDate=response.data.creationDate.substr(0,10);                  
 			myDate=myDate.split("-"); 
 			dateM=myDate[1];
@@ -109,16 +106,12 @@ function expandDiscussion(id){
 			var newDate=finalMonth+" "+myDate[2]+","+myDate[0]; 
 		        console.log("I'm inside Root Message Div");
 		        rootmessage +='<div class="rootborder">';
-			//rootmessage +='<ul>';
 			rootmessage +='<div class="root-header"><a href="'+discussionresult.messages.root.resources.html.ref+'" target="_apps">'+discussionresult.messages.root.subject+'</a></div>';
 			rootmessage +='<div class="content-date"> by <a class="nopad" href=https://apps-onprem.jivesoftware.com/people/'+response.data.author.username+'>'+response.data.author.name+'</a> on '+newDate+'</div>';	
-			rootmessage +='<span class="root">'+discussionresult.messages.root.content.text+'</span>';                             
-                	//rootmessage +=+discussionresult.messages.root.content.text+;                 
+			rootmessage +='<span class="root">'+discussionresult.messages.root.content.text+'</span>';                                        
                         rootmessage +='</div>';				
-			//rootmessage +='</ul>';
 			rootmessage +='</div>';
-			
-			
+		
 			var request = response.data.messages.get( ) ;
 			request.execute(function(response) {
 			var result = response.data;
@@ -134,64 +127,46 @@ function expandDiscussion(id){
 									var finalMonth=monthConvert(dateM);
 									var newDate=finalMonth+" "+myDate[2]+","+myDate[0]; 
 									console.log("I'm inside expand if");
-									correctanswer +='<div class="answerborder">';
-									//correctanswer +='<ul>';
-									correctanswer +='<div class="correct">Correct Answer</div> ';
-									//correctanswer +='<div>by&nbsp;';
-									//correctanswer +='<a class="nopad" href=https://apps-onprem.jivesoftware.com/people/'+row.author.username+'>'+row.author.name+'</a>';
-									//correctanswer +='on&nbsp;'+newDate+'</div>';
-									correctanswer +='<div class="content-date"> by <a class="nopad" href=https://apps-onprem.jivesoftware.com/people/'+row.author.username+'>'+row.author.name+'</a> on '+newDate+'</div>';
-									correctanswer +='<div class="root">'+row.content.text+ '</div	>';
-									//correctanswer +='</ul>';
+									correctanswer +='<div class="answerborder">';								
+									correctanswer +='<div class="correct">Correct Answer</div> ';									
+									correctanswer +='<div class="content-date"> by <a class="nopad" href=https://apps-onprem.jivesoftware.com/people/'+row.author.username+'>'+row.author.name+'</a> on '+newDate+'</div>';									correctanswer +='<div class="root">'+row.content.text+ '</div	>';								
 									correctanswer +='</div>';
-									
-									
-								}
-							if(row.helpful){
+							
+							     }
+							 if(row.helpful){
 									myDate=row.creationDate.substr(0,10);                  
 									myDate=myDate.split("-"); 
 									dateM=myDate[1];
 									var finalMonth=monthConvert(dateM);
 									var newDate=finalMonth+" "+myDate[2]+","+myDate[0]; 
 									console.log("I'm inside expand if");
-									helpfulanswer +='<div class="answerborder">';
-								//	helpfulanswer +='<ul>';
-									helpfulanswer +='<div class="helpful">Helpful Answer </div>';
-									//helfulanswer +='<div>by &nbsp;';
-									//helfulanswer +='<a class="nopad" href=https://apps-onprem.jivesoftware.com/people/'+row.author.username+'>'+row.author.name+'</a>';
-									helpfulanswer +='<div class="content-date"> by <a class="nopad" href=https://apps-onprem.jivesoftware.com/people/'+row.author.username+'>'+row.author.name+'</a> on '+newDate+'</div>';
-									//helpfulanswer +='&nbsp;on&nbsp; '+ newDate + '</div>';
-									helpfulanswer +='<div class="root">'+row.content.text+ '</div>';
-									//helpfulanswer +='</ul>';
+									helpfulanswer +='<div class="answerborder">';							
+									helpfulanswer +='<div class="helpful">Helpful Answer </div>';								
+									helpfulanswer +='<div class="content-date"> by <a class="nopad" href=https://apps-onprem.jivesoftware.com/people/'+row.author.username+'>'+row.author.name+'</a> on '+newDate+'</div>';									
+									helpfulanswer +='<div class="root">'+row.content.text+ '</div>';								
 									helpfulanswer +='</div>';
-									
-									
-								}
-																
-								
+							
+							  }
 					
-					});
+					      });
 					discussionMessage +=rootmessage;
 					discussionMessage +=correctanswer;
 					discussionMessage +=helpfulanswer;
 					console.log("Html Content:: "+discussionMessage);
-								$(".content").show();
-								$(".content").html(discussionMessage);
+					$(".content").show();
+					$(".content").html(discussionMessage);
 				
 				}
-			
 			
 			});
 		
 		}
-	
-	
+
 	});
-	
-	
 
 }
 
+//function for expand button to display the documents
 function expandDocument(id){
 	$(".content").html("");
 	$('.firstdiv').css('background-color', '#FFFFFF');
@@ -221,23 +196,21 @@ function expandDocument(id){
 			}
 			else{
 				if(isBinaryDoc !=0)
-				{       
-						myDate=response.data.creationDate.substr(0,10);                  
-			                        myDate=myDate.split("-"); 
-			                        dateM=myDate[1];
-						var finalMonth=monthConvert(dateM);
-						var newDate=finalMonth+" "+myDate[2]+","+myDate[0]; 
+				  {       
+				        myDate=response.data.creationDate.substr(0,10);                  
+			                myDate=myDate.split("-"); 
+			                dateM=myDate[1];
+					var finalMonth=monthConvert(dateM);
+					var newDate=finalMonth+" "+myDate[2]+","+myDate[0]; 
 					documentdata += '<div class="rootborder">';
 					documentdata += '<span class="root-header"><a href="'+response.data.resources.html.ref+'" target="_app">';						
 					documentdata += response.data.subject+'</a></span>';
-					documentdata +='<div class="content-date"> by <a class="nopad" href=https://apps-onprem.jivesoftware.com/people/'+response.data.author.username+'>'+response.data.author.name+'</a> on '+newDate+'</div></div>';
-					
+					documentdata +='<div class="content-date"> by <a class="nopad" href=https://apps-onprem.jivesoftware.com/people/'+response.data.author.username+'>'+response.data.author.name+'</a> on '+newDate+'</div></div>';				
 					documentdata += '<div class="answerborder root">'+response.data.content.binary.description;
-					//documentdata +='<div class="answerborder">';					
 					documentdata += '<span class="subtext">This document contains an uploaded document (PDF/DOC). ';
 					documentdata += 'Please click <a target="_app" href="'+response.data.resources.html.ref+'">here</a> to open the document</span></div>';
 					documentdata +='</div>';
-				}
+				  }
 				else
 				{
 					myDate=response.data.creationDate.substr(0,10);                  
@@ -249,17 +222,18 @@ function expandDocument(id){
 					documentdata +='<span class="root-header"><a target="_app" href="'+response.data.resources.html.ref+'">';
 					documentdata += response.data.subject+'</a></span>';
 					documentdata +='<div class="content-date"> by <a class="nopad" href=https://apps-onprem.jivesoftware.com/people/'+response.data.author.username+'>'+response.data.author.name+'</a> on '+newDate+'</div>';
-					documentdata +='</div>';
-					
+					documentdata +='</div>';					
 					documentdata +='<div class="answerborder">';
 					documentdata +='<span class="root">'+response.data.content.text +'</span></div>';				
                                         
 				}
-			}
-			$(".content").show();
-			$(".content").html(documentdata);
+			 }
+			 $(".content").show();
+			 $(".content").html(documentdata);
 		});
 }
+
+//function for expand button to display the blog
 function expandBlog(blogId, blogpostId){
 	var postId=blogpostId;
 	var finalpostId=postId.substr(0,postId.indexOf('/'))
@@ -276,40 +250,39 @@ function expandBlog(blogId, blogpostId){
 			request.execute(function(response) {
 				console.log("Posts in blog"+JSON.stringify(response.data));
 				var result = response.data;
-					if(!response.error) {
-						$.each(result, function(index, row) {
-							if(finalpostId.indexOf(row.id) != -1)
-							{
-							var postresult=row.get();
-							postresult.execute(function(response) {
-							console.log("Post Post is"+JSON.stringify(response.data));
-							if (response.error) {
-							console.log("Error in get: "+response.error.message);
-							}
-							else{
-							myDate=response.data.creationDate.substr(0,10);                  
-              myDate=myDate.split("-"); 
-              dateM=myDate[1];
-			        var finalMonth=monthConvert(dateM);
-							var newDate=finalMonth+" "+myDate[2]+","+myDate[0]; 
-							blogdata +='<div class="rootborder">';
-						        blogdata +='<span class="root-header"><a target="_app" href="'+response.data.resources.html.ref+'">';
-							blogdata += response.data.subject+'</a></span>';
-							blogdata +='<div class="content-date"> by <a class="nopad" href=https://apps-onprem.jivesoftware.com/people/'+response.data.author.username+'>'+response.data.author.name+'</a> on '+newDate+'</div>';
-							blogdata +='</div>';
-							
-							blogdata +='<div class="answerborder">';
-							blogdata +='<span class="root">'+response.data.content.text +'</span></div>';	
-							}
-							$(".content").show();
-							$(".content").html(blogdata);
-						});
+				if(!response.error) {
+				   $.each(result, function(index, row) {
+			           if(finalpostId.indexOf(row.id) != -1)
+				     {
+				       var postresult=row.get();
+				       postresult.execute(function(response) {
+				       console.log("Post Post is"+JSON.stringify(response.data));
+				       if (response.error) {
+					   console.log("Error in get: "+response.error.message);
+				          }
+				      else{
+				          myDate=response.data.creationDate.substr(0,10);                  
+                                          myDate=myDate.split("-"); 
+                                          dateM=myDate[1];
+			                  var finalMonth=monthConvert(dateM);
+				          var newDate=finalMonth+" "+myDate[2]+","+myDate[0]; 			    
+					  blogdata +='<div class="rootborder">';
+				          blogdata +='<span class="root-header"><a target="_app" href="'+response.data.resources.html.ref+'">';
+					  blogdata += response.data.subject+'</a></span>';
+					  blogdata +='<div class="content-date"> by <a class="nopad" href=https://apps-onprem.jivesoftware.com/people/'+response.data.author.username+'>'+response.data.author.name+'</a> on '+newDate+'</div>';
+					  blogdata +='</div>';							
+					  blogdata +='<div class="answerborder">';
+					  blogdata +='<span class="root">'+response.data.content.text +'</span></div>';	
+					  }
+					  $(".content").show();
+					  $(".content").html(blogdata);
+				   });
 
 
-					}
+			        }
 
 
-				});
+			    });
 
 			}
 
@@ -317,12 +290,13 @@ function expandBlog(blogId, blogpostId){
 	});
 
 }
+
 // Perform a search and display the results
 function search() {
     
     $("search-results").html("");
-	$(".content").html("");
-	$(".content").hide();
+    $(".content").html("");
+    $(".content").hide();
     gadgets.window.adjustHeight();
     var html ="";
     var params = {
@@ -330,26 +304,23 @@ function search() {
         query : $("#query").val(),
         //sort : $("#sort-type").val(),
        // sortOrder : $("#sort-order").val()
-        
-        
     };
 
-   
     console.log("searching for " + JSON.stringify(params));
     osapi.jive.core.searches.searchContent(params).execute(function(response) {
-       console.log("searching response is " + JSON.stringify(response));
+    console.log("searching response is " + JSON.stringify(response));
        
         if (response.error) {
             alert(response.error.message);
-        }
+           }
         else {
             var html = "";
-			var blog="";
-			var discussion="";
-			var update="";
-			var document="";
-			var post="";
-			
+	    var blog="";
+	    var discussion="";
+	    var update="";
+	    var document="";
+	    var post="";
+				
             var rows = response.data;
             var url="";
             var subject="";
@@ -362,12 +333,13 @@ function search() {
             var type="";
             var username="";
             var myDate="";
-			var isAnswered = 0;
-			var isQuestion = 0
+            
+	    var isAnswered = 0;		
+	    var isQuestion = 0
          
             $.each(rows, function(index, row) {
             	url=row.resources.html.ref;
-				subject=row.subject;
+		subject=row.subject;
                	contentSummary=row.contentSummary;
                 author=row.author.name;
                 createdDate=row.creationDate;                   
@@ -377,7 +349,7 @@ function search() {
                 avatar=row.author.avatarURL;
                 username=row.author.username;
 				
-				try {
+		               try {
 					if (row.question) {
 						isQuestion = 1;
 					}
@@ -400,15 +372,14 @@ function search() {
 				catch (err) {
 					isAnswered = 0;
 				}
-                myDate=row.modificationDate.substr(0,10);                  
-                myDate=myDate.split("-"); 
-                dateM=myDate[1];
-				var finalMonth=monthConvert(dateM);
-				var newDate=finalMonth+" "+myDate[2]+","+myDate[0]; 
-
-
-                         if(row.type=="discussion")
-               	           {
+                 myDate=row.modificationDate.substr(0,10);                  
+                 myDate=myDate.split("-"); 
+                 dateM=myDate[1];
+		 var finalMonth=monthConvert(dateM);
+		  var newDate=finalMonth+" "+myDate[2]+","+myDate[0]; 
+		  
+                             if(row.type=="discussion")
+               	                {
 				var discussionID = (url.substring(url.lastIndexOf("/"))).substr(1);
 				var discussionImage="";
 				if(isQuestion)
@@ -457,6 +428,7 @@ function search() {
 			 if(row.type=="document")
                            {
 				var docID = (url.substring(url.lastIndexOf("-"))).substr(1);
+				
                     		document +='<div id="div_'+docID+'" class="firstdiv"> ';
 				document +='<ul>';
                     		document +='<span class="jive-icon-med jive-icon-document"></span><li> <a href="'+url+'" target="_apps">'+subject+'</a></li>';
@@ -490,13 +462,14 @@ function search() {
 			var blogSummaryId=row.blogSummary.resources.self.ref;
 			var blogId = (blogSummaryId.substring(blogSummaryId.lastIndexOf("/"))).substr(1);
 			var postId = (postDetailsId.substring(postDetailsId.lastIndexOf("/"))).substr(1);
-                    	  post +='<div id="div_'+postId+'" class="firstdiv"> ';
-			  post +='<ul>';
-                          post +='<span class="jive-icon-med jive-icon-blog"></span><li class="post" ><a href="'+url+'" target="_apps">'+subject+'</a></li>';
-                          post +='</ul>';
-                           post +='<ul>';
-                          post +='<span class="jive-icon-med image-button" id="post-'+postId+'/'+blogId+'" ></span>';                            
-                          post +='</ul>';
+			
+                    	 post +='<div id="div_'+postId+'" class="firstdiv"> ';
+			 post +='<ul>';
+                         post +='<span class="jive-icon-med jive-icon-blog"></span><li class="post" ><a href="'+url+'" target="_apps">'+subject+'</a></li>';
+                         post +='</ul>';
+                         post +='<ul>';
+                         post +='<span class="jive-icon-med image-button" id="post-'+postId+'/'+blogId+'" ></span>';                            
+                         post +='</ul>';
                     
 		     	 post +='<div class="root1">'; 
                          post +='<ul>';
@@ -518,17 +491,13 @@ function search() {
                                   
             });
                        
-                       // html +=discussion;
-			//html +=document;
-			//html +=post;
-				
-            console.log(html);
+                 
+             console.log(html);
              $("#tabs-1").html(discussion);
-              $("#tabs-2").html(document);
+             $("#tabs-2").html(document);
              $("#tabs-3").html(post);
-           // $("#search-results").html(html);
-            $("#search-info").show();
-            gadgets.window.adjustHeight();
+             $("#search-info").show();
+             gadgets.window.adjustHeight();
         }
     });
 }
