1532c1213501ea5e9818058acfe732e555c06267
mamatha <munivelu.mamatha@accenture.com>
2012-10-31 11:30:11 +0530

    Update javascripts/search-content.js

--- javascripts/search-content.js
+++ javascripts/search-content.js
@@ -509,13 +509,9 @@ function search() {
 	     
             
              $("#tabs-1").html(all);
-             $("#tabs-2").html(discussion){
-             $('#div_'+id).css('background-color', '#F2F2F2');
-             
-             };
+             $("#tabs-2").html(discussion);                     
              $("#tabs-3").html(document);              
-             $("#tabs-4").html(post);
-             
+             $("#tabs-4").html(post);             
              $("#search-info").show();
              
              gadgets.window.adjustHeight();
