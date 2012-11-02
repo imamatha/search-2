c07183929e746c98f27e057725d07aa54da30a2e
mamatha <munivelu.mamatha@accenture.com>
2012-11-02 13:15:07 +0530

    Update javascripts/search-content.js

--- javascripts/search-content.js
+++ javascripts/search-content.js
@@ -3,6 +3,23 @@ function init() {
       $("#search").click(search);
       gadgets.window.adjustHeight();
 }
+$(document).ready(function(){
+ 
+         //on window scroll fire it will call a function.
+
+                $(window).scroll(function () {
+ 
+         //after window scroll fire it will add define pixel added to that element.
+
+                    set = $(document).scrollTop()+"px";
+ 
+        //this is the jQuery animate function to fixed the div position after scrolling.
+
+                    $('#floatDiv').animate({top:set},{duration:1000,queue:false});
+
+                });
+
+            });
 
 //onhover event of expand icon
 $("span.image-button").live('mouseover', function () {
