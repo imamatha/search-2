7c8a8933cd7dbbbadc656264c2ad04e8b4fdc799
mamatha <munivelu.mamatha@accenture.com>
2012-11-06 12:15:20 +0530

    Update javascripts/search-content.js

--- javascripts/search-content.js
+++ javascripts/search-content.js
@@ -3,23 +3,6 @@ function init() {
       $("#search").click(search);
       gadgets.window.adjustHeight();
 }
-$(document).ready(function(){
- 
-         //on window scroll fire it will call a function.
- 
-                $(window).scroll(function () {
- 
-         //after window scroll fire it will add define pixel added to that element.
- 
-                    set = $(document).scrollTop()+"px";
- 
-        //this is the jQuery animate function to fixed the div position after scrolling.
-                    $('#content').animate({top:set},{duration:1000,queue:false});
- 
-                });
- 
-            });
- 
 
 	
 //	$(function () {
@@ -119,6 +102,23 @@ $("span.image-button").live('mouseover', function () {
        
 //function for expand button to display the discussions with correct and helpful answers
 function expandDiscussion(id){
+         $(document).ready(function(){
+ 
+         //on window scroll fire it will call a function.
+ 
+                $(window).scroll(function () {
+ 
+         //after window scroll fire it will add define pixel added to that element.
+ 
+                    set = $(document).scrollTop()+"px";
+ 
+        //this is the jQuery animate function to fixed the div position after scrolling.
+                    $('#content').animate({top:set},{duration:1000,queue:false});
+ 
+                });
+ 
+            });
+ 
 
 	$("#content").html("");
 	$('.firstdiv').css('background-color', '#FFFFFF');
